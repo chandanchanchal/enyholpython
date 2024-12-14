41dbbe87c9fee006bc451f34626579a2ccaf0192
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-12-14 17:31:54 +0530

    Create testnumpy.py

---
+++ testnumpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array = np.arange(1,11)
+print(array)
+print('####################')
+print(array.shape)
+print('####################')
+print(array.ndim)
+print('####################')
+print(array.size)
